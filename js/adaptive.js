f1f274fc38248c10c165bd6de063e5011e294bde
Hafrona <2864147673@qq.com>
2019-11-05 18:43:05 +0800

    更新

--- js/adaptive.js
+++ js/adaptive.js
@@ -10,5 +10,4 @@ function getRem(pwidth, prem) {
     var html = document.getElementsByTagName("html")[0];
     var oWidth = document.body.clientWidth || document.documentElement.clientWidth;
     html.style.fontSize = oWidth / pwidth * prem + "px";
-    
 }
